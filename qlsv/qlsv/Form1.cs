02b0012392c72658861b72a504384737844b3959
minhtlm <minhtlm1996@gmail.com>
2023-12-06 14:11:42 +0700

    giao dien

--- qlsv/qlsv/Form1.cs
+++ qlsv/qlsv/Form1.cs
@@ -6,5 +6,25 @@ namespace qlsv
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
