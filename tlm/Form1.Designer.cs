2bbfb64a0e665cf37ed1a5d39f4593facb71b7d5
minhtlm <minhtlm1996@gmail.com>
2023-12-07 11:15:07 +0700

    upload

---
+++ tlm/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace tlm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Clear_btn = new System.Windows.Forms.Button();
+            this.Xoa_btn = new System.Windows.Forms.Button();
+            this.HoTen_txt = new System.Windows.Forms.TextBox();
+            this.Sua_btn = new System.Windows.Forms.Button();
+            this.Them_btn = new System.Windows.Forms.Button();
+            this.NgaySinh_dtp = new System.Windows.Forms.DateTimePicker();
+            this.Email_txt = new System.Windows.Forms.TextBox();
+            this.SDT_txt = new System.Windows.Forms.TextBox();
+            this.DiaChi_txt = new System.Windows.Forms.TextBox();
+            this.GioiTinh_txt = new System.Windows.Forms.TextBox();
+            this.MSSV_txt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tlm_dtg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tlm_dtg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Clear_btn
+            // 
+            this.Clear_btn.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Clear_btn.FlatAppearance.BorderSize = 0;
+            this.Clear_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear_btn.ForeColor = System.Drawing.Color.Black;
+            this.Clear_btn.Location = new System.Drawing.Point(159, 318);
+            this.Clear_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.Clear_btn.Name = "Clear_btn";
+            this.Clear_btn.Size = new System.Drawing.Size(96, 38);
+            this.Clear_btn.TabIndex = 29;
+            this.Clear_btn.Text = "Clear";
+            this.Clear_btn.UseVisualStyleBackColor = false;
+            this.Clear_btn.Click += new System.EventHandler(this.Clear_btn_Click);
+            // 
+            // Xoa_btn
+            // 
+            this.Xoa_btn.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Xoa_btn.FlatAppearance.BorderSize = 0;
+            this.Xoa_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Xoa_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Xoa_btn.ForeColor = System.Drawing.Color.Black;
+            this.Xoa_btn.Location = new System.Drawing.Point(30, 318);
+            this.Xoa_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.Xoa_btn.Name = "Xoa_btn";
+            this.Xoa_btn.Size = new System.Drawing.Size(96, 38);
+            this.Xoa_btn.TabIndex = 28;
+            this.Xoa_btn.Text = "Xóa";
+            this.Xoa_btn.UseVisualStyleBackColor = false;
+            this.Xoa_btn.Click += new System.EventHandler(this.Xoa_btn_Click);
+            // 
+            // HoTen_txt
+            // 
+            this.HoTen_txt.Location = new System.Drawing.Point(98, 64);
+            this.HoTen_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.HoTen_txt.Name = "HoTen_txt";
+            this.HoTen_txt.Size = new System.Drawing.Size(153, 20);
+            this.HoTen_txt.TabIndex = 14;
+            // 
+            // Sua_btn
+            // 
+            this.Sua_btn.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Sua_btn.Enabled = false;
+            this.Sua_btn.FlatAppearance.BorderSize = 0;
+            this.Sua_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Sua_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sua_btn.ForeColor = System.Drawing.Color.Black;
+            this.Sua_btn.Location = new System.Drawing.Point(159, 256);
+            this.Sua_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.Sua_btn.Name = "Sua_btn";
+            this.Sua_btn.Size = new System.Drawing.Size(96, 38);
+            this.Sua_btn.TabIndex = 27;
+            this.Sua_btn.Text = "Sửa";
+            this.Sua_btn.UseVisualStyleBackColor = false;
+            this.Sua_btn.Click += new System.EventHandler(this.Sua_btn_Click);
+            // 
+            // Them_btn
+            // 
+            this.Them_btn.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Them_btn.FlatAppearance.BorderSize = 0;
+            this.Them_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Them_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Them_btn.ForeColor = System.Drawing.Color.Black;
+            this.Them_btn.Location = new System.Drawing.Point(30, 256);
+            this.Them_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.Them_btn.Name = "Them_btn";
+            this.Them_btn.Size = new System.Drawing.Size(96, 38);
+            this.Them_btn.TabIndex = 26;
+            this.Them_btn.Text = "Thêm mới";
+            this.Them_btn.UseVisualStyleBackColor = false;
+            this.Them_btn.Click += new System.EventHandler(this.Them_btn_Click);
+            // 
+            // NgaySinh_dtp
+            // 
+            this.NgaySinh_dtp.Location = new System.Drawing.Point(98, 129);
+            this.NgaySinh_dtp.Margin = new System.Windows.Forms.Padding(2);
+            this.NgaySinh_dtp.Name = "NgaySinh_dtp";
+            this.NgaySinh_dtp.Size = new System.Drawing.Size(153, 20);
+            this.NgaySinh_dtp.TabIndex = 19;
+            // 
+            // Email_txt
+            // 
+            this.Email_txt.Location = new System.Drawing.Point(98, 220);
+            this.Email_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.Email_txt.Name = "Email_txt";
+            this.Email_txt.Size = new System.Drawing.Size(153, 20);
+            this.Email_txt.TabIndex = 25;
+            // 
+            // SDT_txt
+            // 
+            this.SDT_txt.Location = new System.Drawing.Point(98, 190);
+            this.SDT_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.SDT_txt.Name = "SDT_txt";
+            this.SDT_txt.Size = new System.Drawing.Size(153, 20);
+            this.SDT_txt.TabIndex = 24;
+            // 
+            // DiaChi_txt
+            // 
+            this.DiaChi_txt.Location = new System.Drawing.Point(98, 158);
+            this.DiaChi_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.DiaChi_txt.Name = "DiaChi_txt";
+            this.DiaChi_txt.Size = new System.Drawing.Size(153, 20);
+            this.DiaChi_txt.TabIndex = 23;
+            // 
+            // GioiTinh_txt
+            // 
+            this.GioiTinh_txt.Location = new System.Drawing.Point(98, 93);
+            this.GioiTinh_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.GioiTinh_txt.Name = "GioiTinh_txt";
+            this.GioiTinh_txt.Size = new System.Drawing.Size(153, 20);
+            this.GioiTinh_txt.TabIndex = 17;
+            // 
+            // MSSV_txt
+            // 
+            this.MSSV_txt.Location = new System.Drawing.Point(98, 32);
+            this.MSSV_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.MSSV_txt.Name = "MSSV_txt";
+            this.MSSV_txt.Size = new System.Drawing.Size(153, 20);
+            this.MSSV_txt.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 220);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 190);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Số điện thoại";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 158);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Địa chỉ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 129);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Ngày Sinh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Giới tính";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Họ và tên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "MSSV";
+            // 
+            // tlm_dtg
+            // 
+            this.tlm_dtg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlm_dtg.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tlm_dtg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tlm_dtg.Location = new System.Drawing.Point(269, 2);
+            this.tlm_dtg.Margin = new System.Windows.Forms.Padding(2);
+            this.tlm_dtg.Name = "tlm_dtg";
+            this.tlm_dtg.RowHeadersWidth = 62;
+            this.tlm_dtg.RowTemplate.Height = 28;
+            this.tlm_dtg.Size = new System.Drawing.Size(701, 362);
+            this.tlm_dtg.TabIndex = 30;
+            this.tlm_dtg.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tlm_dtg_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 374);
+            this.Controls.Add(this.tlm_dtg);
+            this.Controls.Add(this.Clear_btn);
+            this.Controls.Add(this.Xoa_btn);
+            this.Controls.Add(this.HoTen_txt);
+            this.Controls.Add(this.Sua_btn);
+            this.Controls.Add(this.Them_btn);
+            this.Controls.Add(this.NgaySinh_dtp);
+            this.Controls.Add(this.Email_txt);
+            this.Controls.Add(this.SDT_txt);
+            this.Controls.Add(this.DiaChi_txt);
+            this.Controls.Add(this.GioiTinh_txt);
+            this.Controls.Add(this.MSSV_txt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tlm_dtg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Clear_btn;
+        private System.Windows.Forms.Button Xoa_btn;
+        private System.Windows.Forms.TextBox HoTen_txt;
+        private System.Windows.Forms.Button Sua_btn;
+        private System.Windows.Forms.Button Them_btn;
+        private System.Windows.Forms.DateTimePicker NgaySinh_dtp;
+        private System.Windows.Forms.TextBox Email_txt;
+        private System.Windows.Forms.TextBox SDT_txt;
+        private System.Windows.Forms.TextBox DiaChi_txt;
+        private System.Windows.Forms.TextBox GioiTinh_txt;
+        private System.Windows.Forms.TextBox MSSV_txt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView tlm_dtg;
+    }
+}
+
---
+++ tlm/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tlm
+{
+    public partial class Form1 : Form
+    {
+        tlmDataContext db = new tlmDataContext();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load()
+        {
+            List<tbl_SinhVien> dssv = new List<tbl_SinhVien>();
+            dssv = db.tbl_SinhViens.ToList();
+            tlm_dtg.DataSource = dssv;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void Them_btn_Click(object sender, EventArgs e)
+        {
+            tbl_SinhVien newObj = new tbl_SinhVien();
+            newObj.MSSV = MSSV_txt.Text;
+            newObj.HoTen = HoTen_txt.Text;
+            newObj.GioiTinh = GioiTinh_txt.Text;
+            newObj.NgaySinh = Convert.ToString(NgaySinh_dtp.Value);
+            newObj.DiaChi = DiaChi_txt.Text;
+            newObj.SDT = SDT_txt.Text;
+            newObj.Email = Email_txt.Text;
+            db.tbl_SinhViens.InsertOnSubmit(newObj);
+            db.SubmitChanges();
+            MessageBox.Show("Thêm mới thành công!");
+            load();
+        }
+
+        private void Clear_btn_Click(object sender, EventArgs e)
+        {
+            MSSV_txt.Text = "";
+            HoTen_txt.Text = "";
+            GioiTinh_txt.Text = "";
+            NgaySinh_dtp.Text = "";
+            DiaChi_txt.Text = "";
+            SDT_txt.Text = "";
+            Email_txt.Text = "";
+        }
+
+        private void tlm_dtg_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = tlm_dtg.Rows[e.RowIndex];
+            MSSV_txt.Text = row.Cells[0].Value.ToString();
+            HoTen_txt.Text = row.Cells[1].Value.ToString();
+            GioiTinh_txt.Text = row.Cells[2].Value.ToString();
+            NgaySinh_dtp.Text = row.Cells[3].Value.ToString();
+            DiaChi_txt.Text = row.Cells[4].Value.ToString();
+            SDT_txt.Text = row.Cells[5].Value.ToString();
+            Email_txt.Text = row.Cells[6].Value.ToString();
+        }
+
+        private void Sua_btn_Click(object sender, EventArgs e)
+        {
+            tbl_SinhVien suaObj = db.tbl_SinhViens.Where(d=> d.MSSV.Equals(MSSV_txt.Text)).FirstOrDefault();
+            suaObj.MSSV = MSSV_txt.Text;
+            suaObj.HoTen = HoTen_txt.Text;
+            suaObj.GioiTinh = GioiTinh_txt.Text;
+            suaObj.NgaySinh = Convert.ToString(NgaySinh_dtp.Value);
+            suaObj.DiaChi= DiaChi_txt.Text;
+            suaObj.SDT = SDT_txt.Text;
+            suaObj.Email= Email_txt.Text;
+            db.SubmitChanges();
+            MessageBox.Show("Sửa thành công!");
+            load();
+        }
+
+        private void Xoa_btn_Click(object sender, EventArgs e)
+        {
+            tbl_SinhVien xoaObj = db.tbl_SinhViens.Where(p => p.MSSV.Equals(MSSV_txt.Text)).FirstOrDefault();
+            db.tbl_SinhViens.DeleteOnSubmit(xoaObj);
+            db.SubmitChanges();
+            MessageBox.Show("Xóa thành công!");
+            load();
+        }
+    }
+}
